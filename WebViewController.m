64a3911331db909def3e5b748abb94c84da46d9d
Joseph Fabisevich <mergesort@users.noreply.github.com>
2014-09-07 09:04:17 -0400

    Never leave your testing code in your release...

--- WebViewController.m
+++ WebViewController.m
@@ -138,10 +138,6 @@ CGFloat const WebViewControllertoolbarHeight = 44.0f;
     self.webView.scrollView.delegate = self;
 
     [self setBrowserButtonState];
-
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        self.URL = [NSURL URLWithString:@"http://www.xkcd.com"];
-    });
 }
 
 - (void)setupConstraints
